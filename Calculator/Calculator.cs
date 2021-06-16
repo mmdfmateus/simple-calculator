2ead5bce1b575af1ac81d31d9906ca279e0b5d66
Mateus Mourão <mateusm@take.net>
2021-06-16 01:42:19 -0300

    first commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public double Divide(int number1, int number2)
+        {
+            if(number2 == 0)
+            {
+                throw new Exception("Não existe divisão por 0");  // Não existe divisão por 0
+            }
+            return number1 / number2;
+        }
+    }
+}
